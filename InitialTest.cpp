27825a86baa99cc985505ef0bb16b60fabbff306
Akshaya Pathipati <akshayapathipati2008@gmail.com>
2026-01-26 20:49:12 -0800

    Initial commit

---
+++ InitialTest.cpp
@@ -0,0 +1,11 @@
+//
+// Created by aksha on 1/26/2026.
+//
+
+#include "InitialTest.h"
+#include <iostream>
+
+int main() {
+    std::cout << "Mission Control is ready!" << std::endl;
+    return 0;
+}
---
+++ InitialTest.h
@@ -0,0 +1,13 @@
+//
+// Created by aksha on 1/26/2026.
+//
+
+#ifndef CS210_SPRING2026_INITIALTEST_H
+#define CS210_SPRING2026_INITIALTEST_H
+
+
+class InitialTest {
+};
+
+
+#endif //CS210_SPRING2026_INITIALTEST_H
