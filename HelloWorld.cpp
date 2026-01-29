45057aa03153a52df4703c40cc5247cae1aad8c7
Akshaya Pathipati <akshayapathipati2008@gmail.com>
2026-01-28 20:21:59 -0800

    Hello World Program

---
+++ HelloWorld.cpp
@@ -0,0 +1,11 @@
+//
+// Created by aksha on 1/28/2026.
+//
+
+#include "HelloWorld.h"
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Hello World!" << endl;
+}
---
+++ HelloWorld.h
@@ -0,0 +1,13 @@
+//
+// Created by aksha on 1/28/2026.
+//
+
+#ifndef CS210_SPRING2026_HELLOWORLD_H
+#define CS210_SPRING2026_HELLOWORLD_H
+
+
+class HelloWorld {
+};
+
+
+#endif //CS210_SPRING2026_HELLOWORLD_H
